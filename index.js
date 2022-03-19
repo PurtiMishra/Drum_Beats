6ed047439d9dc44f5715e3dc3f3a46a68f9f0288
Purti Mishra <91051576+PurtiMishra@users.noreply.github.com>
2022-03-19 08:16:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+document.querySelector(".w").addEventListener("click",function(){
+    var audio1=new Audio("sounds/tom-1.mp3"); 
+    audio1.play();
+
+})
+document.querySelector(".a").addEventListener("click",function(){
+    var audio2=new Audio("sounds/tom-2.mp3"); 
+    audio2.play();
+
+})
+document.querySelector(".s").addEventListener("click",function(){
+    var audio2=new Audio("sounds/tom-3.mp3"); 
+    audio2.play();
+
+})
+document.querySelector(".d").addEventListener("click",function(){
+    var audio2=new Audio("sounds/tom-4.mp3"); 
+    audio2.play();
+
+})
+document.querySelector(".j").addEventListener("click",function(){
+    var audio2=new Audio("sounds/snare.mp3"); 
+    audio2.play();
+
+})
+document.querySelector(".k").addEventListener("click",function(){
+    var audio2=new Audio("sounds/crash.mp3"); 
+    audio2.play();
+
+})
+document.querySelector(".l").addEventListener("click",function(){
+    var audio2=new Audio("sounds/kick-bass.mp3"); 
+    audio2.play();
+
+})
